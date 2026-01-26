55d5e3738e7bc43a91dc6f523d4106f276dfab08
malaka96 <madhubhashana655@gmail.com>
2026-01-26 20:19:11 +0530

    created controller,service for Users

---
+++ src/main/java/edu/malaka96/controllers/UsersController.java
@@ -0,0 +1,21 @@
+package edu.malaka96.controllers;
+
+import edu.malaka96.model.Users;
+import edu.malaka96.service.UsersService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController //@ResponseBody + @Controller + @Component
+@RequiredArgsConstructor
+public class UsersController {
+
+    final UsersService usersService;
+
+    @GetMapping("/users/all")
+    public List<Users> getUsers(){
+        return usersService.getAllUsers();
+    }
+}
---
+++ src/main/java/edu/malaka96/service/UsersService.java
@@ -0,0 +1,19 @@
+package edu.malaka96.service;
+
+import edu.malaka96.model.Users;
+import edu.malaka96.repository.UserRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+@RequiredArgsConstructor
+public class UsersService {
+    final UserRepository userRepository;
+
+    public List<Users> getAllUsers(){
+        return userRepository.findAll();
+    }
+
+}
